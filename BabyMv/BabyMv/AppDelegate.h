7da99221494cb06ab823ee30843a00cf95b6c6ac
snowflyingshan <shanyongjie2004@163.com>
2016-02-05 05:07:01 +0800

    Add: 创建 single view application；

---
+++ BabyMv/BabyMv/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BabyMv
+//
+//  Created by 单永杰 on 16/2/5.
+//  Copyright © 2016年 chenjingying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BabyMv/BabyMv/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BabyMv
+//
+//  Created by 单永杰 on 16/2/5.
+//  Copyright © 2016年 chenjingying. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
